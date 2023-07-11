269ae1a8e04afe3f86b90ce8654d063d0b83f153
JohnDavidSmith <128341208+JohnDavidSmith@users.noreply.github.com>
2023-07-11 08:30:17 -0400

    New Files

---
+++ README.md
@@ -0,0 +1,48 @@
+# 21 MERN: Book Search Engine
+
+## Description 
+
+* The main goal of this project is to take starter code with a fully functioning Google Books API search engine built with a RESTful API, and refactor it to be a GraphQL API built with Apollo Server. 
+
+* Most modern websites are driven by two things: data and user demands. This shouldn't come as a surprise, as the ability to personalize user data is the cornerstone of real-world web development today.
+
+## Contents 
+
+* This project built using the MERN stack with a React front end, MongoDB database, and Node.js/Express.js server and API. It's already set up to allow users to save book searches to the back end.  
+
+## Installation 
+
+* Doesn't need any instillation just run "npm i" in the command line then run "npm run develop" then you can have the App.
+  
+## Usage 
+
+* This application can be used by anyone wants to have the ability to personalize user data is the cornerstone of real-world web development today. And as user demands evolve, applications need to be more performant.    
+
+## Screenshot
+
+![Screenshot of the React Portfolio](./src/Assets/Screenshot%20React%20Portfolio.png)
+
+## GitHub link
+
+* [The React Portfolio](https://github.com/JohnDavidSmith/ReactPortfolio-React-Mod20)
+
+## Heroku link
+* [The React Portfolio](https://github.com/JohnDavidSmith/ReactPortfolio-React-Mod20)
+
+## Credits
+* 
+
+## License
+* 
+
+## Badges
+* 
+
+## Features
+* 
+
+## How to Contribute
+* 
+
+## Test
+*
---
+++ schemas/resolvers.js
@@ -0,0 +1,67 @@
+const { User, Book } = require('../server/models');
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../server/utils/auth');
+
+const resolvers = {
+  Query: {
+    me: async (_, __, context) => {
+      if (context.user) {
+        const user = await User.findById(context.user._id).populate('savedBooks');
+        return user;
+      }
+
+      throw new AuthenticationError('You need to be logged in to perform this action');
+    },
+  },
+  Mutation: {
+    login: async (_, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError('Invalid email or password');
+      }
+
+      const correctPassword = await user.isCorrectPassword(password);
+
+      if (!correctPassword) {
+        throw new AuthenticationError('Invalid email or password');
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+    addUser: async (_, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
+    },
+    saveBook: async (_, { input }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findByIdAndUpdate(
+          context.user._id,
+          { $addToSet: { savedBooks: input } },
+          { new: true }
+        ).populate('savedBooks');
+
+        return updatedUser;
+      }
+
+      throw new AuthenticationError('You need to be logged in to perform this action');
+    },
+    removeBook: async (_, { bookId }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findByIdAndUpdate(
+          context.user._id,
+          { $pull: { savedBooks: { bookId } } },
+          { new: true }
+        ).populate('savedBooks');
+
+        return updatedUser;
+      }
+
+      throw new AuthenticationError('You need to be logged in to perform this action');
+    },
+  },
+};
+
+module.exports = resolvers;
